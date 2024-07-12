b6ff2befc2c6215aa03eaceb54f6bd54f2483a55
acdlvvq <yungggblessed@gmail.com>
2024-07-12 17:02:03 +0300

    Create BirthdayCongratulationsSender Background Service

--- Clinic/Clinic.API/Program.cs
+++ Clinic/Clinic.API/Program.cs
@@ -3,6 +3,10 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Clinic.Domain.Interfaces;
 using Clinic.Domain.Repositories;
+using Clinic.Application.BackgroundServices;
+using Clinic.API.Settings;
+using System.Net.Mail;
+using System.Net;
 
 namespace Clinic.API;
 
@@ -11,6 +15,9 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var smtp = builder.Configuration
+            .GetSection("SmtpSettings")
+            .Get<Smtp>();
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
@@ -32,9 +39,19 @@ public class Program
             .AddScoped<IPatientsRepository, PatientsRepository>()
             .AddScoped<IPhotosRepository, PhotosRepository>()
             .AddScoped<IReceptionistsRepository, ReceptionistsRepository>();
+        builder.Services
+            .AddFluentEmail(smtp!.FromEmail, smtp.FromName)
+            .AddSmtpSender(new SmtpClient(smtp.Host)
+            { 
+                Port = smtp.Port,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(smtp.Username, smtp.Password),
+            });
 
         builder.Services.AddMediatR(
             cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        builder.Services.AddHostedService<BirthdayCongratulationsSender>();
 
         var app = builder.Build();
         if (app.Environment.IsDevelopment())
